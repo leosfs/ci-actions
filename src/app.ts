e7f763080a9075e88dd8c3009611d2e41d657442
Leonardo Henrique Dos Santos Ferreira Da Silva <leonardo.silva@mobile.local>
2023-06-20 09:49:05 -0300

    init project

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { NextFunction, Request, Response } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/ci", (req, res) => {
+    res.json({message:true})
+})
+
+export { app };
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+
+
+import { app } from './app';
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Running at http://localhost:${PORT}`);
+});
