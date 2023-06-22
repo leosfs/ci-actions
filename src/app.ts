1377dfd5f80151e2f757a347b19ab94772a69d51
leosfs <leonardo.silva@escolamobile.com.br>
2023-06-22 07:11:59 -0300

    feat: View screen

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 import express, { NextFunction, Request, Response } from 'express';
+import figlet from "figlet"
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/ci", (req, res) => {
-    res.json({message:true, espCode:"42-es"})
+app.get("/", (req, res) => {
+figlet("SFS-Memory -><-", function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+    res.json({message:"SFS-Memory -><-"});
+    // res.redirect(data)
+
+  });
+
 })
 
 export { app };
