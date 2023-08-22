e3b118e3eaa4de69b607617d00939bf3e9733602
Leonardo <137170387+leosfs@users.noreply.github.com>
2023-08-22 09:35:06 -0300

    teste ci

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ import { app } from './app';
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Running at the http://localhost:${PORT}`);
+  console.log(`Running at the http://localhost:${PORT}/teste`);
 });
